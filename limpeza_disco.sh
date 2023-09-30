94d1968e84db409b56aca76f3769ce4c34c0fc3f
Ronaldo192 <80844881+Ronaldo192@users.noreply.github.com>
2023-09-30 20:14:48 -0300

    Add files via upload

---
+++ limpeza_disco.sh
@@ -0,0 +1,23 @@
+#!/urs/bin/env bash
+
+limite_ocupacao=70
+
+# Obtém a ocupação em disco da partição raiz (/) 
+# tratamento da string pegando a segunda linha 6 item e abstraindo o simbulo de %
+ocupacao=$(df -h / | awk 'NR==2 {print $6}' | tr -d '%')
+
+# #condicional baseado no limite de ocupação,
+Verifica se a ocupação está acima do limite definido
+if [ "$ocupacao" -gt "$limite_ocupacao" ]; then
+    echo "A ocupação em disco está acima de $limite_ocupacao%."
+    echo "Executando 'docker system prune' para liberar espaço..."
+
+    # Executa seu comando de limpeza
+    # Executa comandos adicionais 
+
+
+    echo "Limpeza do sistema Docker concluída."
+else
+    echo "A ocupação em disco está abaixo de $limite_ocupacao%."
+    echo "Nenhuma ação é necessária neste momento."
+fi
